3b469eda5f4cdbc4bd5a09413b745f1e0c5b6b6a
malinovaya0iriska <malinovayairiska@gmail.com>
2021-10-05 20:36:18 +0300

    Updates

---
+++ precache-manifest.5e30ca841cbddea3f1c94c423a46094c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fe1633481704861f78d3bb1487ec40",
+    "url": "/react-homeworks/index.html"
+  },
+  {
+    "revision": "43799e133fab228a40fe",
+    "url": "/react-homeworks/static/css/main.37b5cdca.chunk.css"
+  },
+  {
+    "revision": "5e955228718f5a7c25b0",
+    "url": "/react-homeworks/static/js/2.a4d06851.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-homeworks/static/js/2.a4d06851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43799e133fab228a40fe",
+    "url": "/react-homeworks/static/js/main.420f20fb.chunk.js"
+  },
+  {
+    "revision": "e82a053bb866dd05a324",
+    "url": "/react-homeworks/static/js/runtime-main.8ad1a092.js"
+  },
+  {
+    "revision": "eee6497eb3b97566443273b0485fbac5",
+    "url": "/react-homeworks/static/media/error.eee6497e.gif"
+  },
+  {
+    "revision": "ddafe548608701a0bd495eca759afb47",
+    "url": "/react-homeworks/static/media/feather.ddafe548.png"
+  }
+]);
