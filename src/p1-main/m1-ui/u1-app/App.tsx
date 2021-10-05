f45fca1fc22e5a2a3bde4bdaf720796278374bea
malinovaya0iriska <malinovayairiska@gmail.com>
2021-10-05 20:18:29 +0300

    feat: add HW 4, 5, 6

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import s from './App.module.css'
-import HW5 from "../../../p2-homeworks/h5/HW5";
+import HW4 from "../../../p2-homeworks/h4/HW4";
+import HW2 from "../../../p2-homeworks/h2/HW2";
+import HW3 from "../../../p2-homeworks/h3/HW3";
+import HW1 from "../../../p2-homeworks/h1/HW1";
 
 function App() {
     return (
         <div className={s.App}>
             <div><h1>react homeworks:</h1></div>
-            {/*<HW1/>
+            <HW1/>
             <HW2/>
             <HW3/>
-            <HW4/>*/}
-             <HW5/>
+            <HW4/>
+            {/* <HW5/>*/}
 
         </div>
     )
