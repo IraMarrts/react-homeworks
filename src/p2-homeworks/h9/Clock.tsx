43ecb940ea7a992955d34df71979abffdca4283c
malinovaya0iriska <malinovayairiska@gmail.com>
2021-10-06 22:09:52 +0300

    feat: add HW 9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,46 +1,55 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
+    let formatterTime = new Intl.DateTimeFormat("ru", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    });
+    let formatterDate = new Intl.DateTimeFormat("ru", {
+        /* weekday: "long",*/
+        year: "numeric",
+        month: "numeric", //"long";
+        day: "numeric"
+    });
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
-
+    const stringTime = formatterTime.format(date) // fix with date
+    const stringDate = formatterDate.format(date)// fix with date
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
+            <div className={s.time}
+                 onMouseEnter={onMouseEnter}
+                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+            <div className={s.date}>
+                {show ? stringDate : (<br/>)}
+            </div>
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
